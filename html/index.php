3b60474d4afc4dcd7593e6874f246159f3738b69
enma-29 <felixenmanuel@hotmail.com>
2024-02-12 22:39:01 -0400

    login disenio

---
+++ html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login|Sistema Facturacion</title>
+    <link rel="stylesheet" href="../css/style.css">
+    
+</head>
+<body>
+    <section id="container">
+        <form action="" method="post">
+            <h3>Iniciar sesión</h3>
+            <img src="../img/pngtree.jpg" alt="login">
+
+            <input type="text" name="usuario" placeholder="Usuario" >
+            <input type="password" name="clave" placeholder="Clave" >
+            <p class="alerta"></p>
+            <input type="submit" value="INGRESAR">
+
+        </form>
+    </section>
+   
+</body>
+</html>
